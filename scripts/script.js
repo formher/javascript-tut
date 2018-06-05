052deb657dd23078eebef43d89053d942331dbe3
formher <formher@gmail.com>
2018-06-05 20:34:04 +0200

    bzbzoci

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
 
-var a = 2;
+var a = 6;
 var b = 4;
 var result = a + b;
 
-document.write(a + " + " + b + " = " + result)
+if (a < b){
+    document.write(a + " is less than " + b)
+}
+
+else if (a > b) {
+    document.write(a + " is greater than " + b)
+}
+
+else {
+  document.write(a + " is equal to " + b)
+}
