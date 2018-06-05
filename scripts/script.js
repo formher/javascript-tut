86ea8303a8cd4aee03f12c535249d14ba86eace3
formher <formher@gmail.com>
2018-06-05 23:54:33 +0200

    js

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,9 @@
+var a = document.getElementById('myinput1');
+var b = document.getElementById('myinput2');
 
-var a = prompt("Input first number to compare.");
-var b = prompt("Input second number to compare.");
+var result = a + b;
 
-if (a < b){
-    document.write(a + " is less than " + b)
-}
 
-else if (a > b) {
-    document.write(a + " is greater than " + b)
-}
-
-else {
-  document.write(a + " is equal to " + b)
+function calcResult() {
+  document.getElementById('resultText').textContent = result;
 }
