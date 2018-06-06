96efdcc6cd52dfaadcca09632fa134bfd1e31dcf
formher <formher@gmail.com>
2018-06-06 17:53:52 +0200

    changed JS script

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,8 @@
 function calcResult() {
   var a = document.getElementById('myinput1').value;
   var b = document.getElementById('myinput2').value;
-  var result = a + b;
-  document.getElementById('resultField').textContent = +a + +b;
+  var result = Number(a) + Number(b);
+  document.getElementById('resultField').textContent = "Result is: " + result;
 }
 
 function clearRslt(){
